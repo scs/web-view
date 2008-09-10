c5e1ee6873d905dba588ec2fe23340be446d093d
Samuel Zahnd <samuel.zahnd@scs.ch>
2008-09-10 10:56:03 +0200

    + Use scp to download. No default IP in runapp.sh

--- targetFiles/runapp.sh
+++ targetFiles/runapp.sh
@@ -1,6 +1,3 @@
-ifconfig eth0 192.168.11.10 netmask 255.255.255.0
-route add -net default gw 192.168.11.1 
-
 cp www.tar.gz /home/httpd; 
 gunzip /home/httpd/www.tar.gz;  
 tar xf /home/httpd/www.tar -C /home/httpd;
